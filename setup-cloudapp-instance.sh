3a50e0a56b518ce71f9be5b789df261743869b8f
Hariharan Sundaram <gautham.apa@gmail.com>
2023-10-25 15:25:24 -0400

    Added cloud app service and daemons for automatically launching webapp upon system boot

--- setup-cloudapp-instance.sh
+++ setup-cloudapp-instance.sh
@@ -9,17 +9,12 @@ sudo apt-get install gpgv -y
 sudo apt-get install gnupg -y
 sudo apt-get install unzip -y
 
-
-# Install Postgresql
-sudo sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get -y install postgresql
-
 unzip CloudAppRelease.zip
-cp CloudAppRelease/users.csv /opt/users.csv
+sudo cp CloudAppRelease/users.csv /opt/users.csv
+sudo cp -r CloudAppRelease /opt
+sudo cp CloudAppRelease/cloudapp.service /etc/systemd/system
 wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
 sudo dpkg -i jdk-21_linux-x64_bin.deb 
 
-sudo -u postgres psql
-sudo -i -u postgres psql -d postgres -c "create user hariharansundaram with encrypted password 'gautham123';"
-sudo -i -u postgres psql -d postgres -c "alter database postgres owner to hariharansundaram;"
+sudo groupadd csye6225
+sudo useradd -s /bin/false -g csye6225 -d /opt/CloudAppRelease -m csye6225
